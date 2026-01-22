6be07b4baac7babda9154ecb9edfd987a2dd6b50
harshiniramanan14-hash <harshiniramanan14@gmail.com>
2026-01-22 13:43:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ with st.sidebar:
     enhance_prompt = st.checkbox("Enhance Prompt (adds 'cinematic, 4k')", value=True)
 
 # --- Main UI ---
-st.title("🏍️ MotoGen: AI Bike Generator")
+st.title(" HarshGen: AI Bike Generator")
 st.write("Transform your imagination into high-speed reality.")
 
 prompt = st.text_input("Describe the bike you want to see:", placeholder="e.g. A futuristic neon Ducati in a cyberpunk city")
